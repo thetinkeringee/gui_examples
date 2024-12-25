605713607aef573a47aedba23722cf896c9e9626
Ezekiel Holliday <thetinkeringengineer@protonmail.com>
2024-12-24 19:46:41 -0500

    Refactor xcb/example01

--- xcb/example01/README.md
+++ xcb/example01/README.md
@@ -1,9 +1,10 @@
-# Example 1 A Window
+# XCB Example 1: A Simple Window 
 
-This is a simple example of creating a basic window that will close when escape
+This is a example of creating a basic window that will close when escape
 is pressed. It should be run from the terminal so that the information it
 prints is visible.
 
-> **NOTE** Do not close the window by press the X at the top right corner of the
-> window. This will close the window but not the program. If you do this, press
-> control-C to kill the program. This will be fixed in a different example.
+> **NOTE** Do not close the window by pressing the X at the top right corner of
+> the window. This will close the window but not the program. If you do this,
+> press control-C to kill the program. This issue will be fixed in the next
+> example.
--- xcb/example01/main.c
+++ xcb/example01/main.c
@@ -1,10 +1,38 @@
+/* 
+ * MIT License
+ * 
+ * Copyright (c) 2024 Ezekiel Holliday 
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <xcb/xcb.h>
+#include <xcb/xcb_event.h>
 #include <xcb/xcb_util.h>
 #include <xcb/xproto.h>
+#include "util.h"
 
+// Structure to hold xcb specific information
 static struct {
   xcb_connection_t *connection;
   int32_t screenNumber;
@@ -12,37 +40,44 @@ static struct {
 
 } xcb = {};
 
-// Define the backgrouund color
+
+// Define the Backgrouund Color
 //
-// Alpha will likely have no effect on the display with out
-// other changes to the code. This will be shown in anotehr
-// example.
+// The alpha value will likely have no effect on what displayed without
+// other changes to the code due to likely only have 24 bit depth. A later
+// example will show how to switch to 32 bit depth.
 //
-// ALPHA RED GREEN BLUE
+// NOTE: The oreder of the color chanels are a little different than what is
+// typically used.
+// Chanel order: alpha red green blue
 #define BG_COLOR 0xFF404050
+
 
 int main(void) {
 
-  // This will connect to the default display
+  // This will connect to the default display and screen 0
   xcb.connection = xcb_connect(nullptr, &xcb.screenNumber);
 
-  // get the screen
-  // This function can be repleace with a for loop iterating over
-  // the screens.
+  // Get the screen
+  // This function can be repleaced with a for loop iterating over
+  // the screens, but this function exists to make it easier.
   xcb.screen = xcb_aux_get_screen(xcb.connection, xcb.screenNumber);
 
-  // push all commands to the server
+  // Push all commands to the server
   xcb_flush(xcb.connection);
 
-  //  Check to see if we were able to connect properly
+  //  Check to see if a proper connection was possible 
   if (xcb_connection_has_error(xcb.connection)) {
     fprintf(stderr, "Error with connection to X11 server\n");
     return -1;
   }
 
-  // Print some information just because we can
-  printf("Your screen is %d x %d pixels\n", xcb.screen->width_in_pixels,
+  // Print some information about your display just because we can
+  printf("Your screen is %d x %d pixels\n\n", xcb.screen->width_in_pixels,
          xcb.screen->height_in_pixels);
+
+  //---------------------------------------------------------------------------
+  // Creating the Window
 
   // The value mask specifies what information is being pased in values to the
   // server
@@ -61,7 +96,7 @@ int main(void) {
       XCB_EVENT_MASK_KEY_PRESS // Receive key press events
   };
 
-  // generate an id for our window
+  // Generate an id for our window
   xcb_window_t window1 = xcb_generate_id(xcb.connection);
 
   xcb_create_window(xcb.connection,       // connection to the X11 server
@@ -73,16 +108,14 @@ int main(void) {
                     400,                  // Window width 
                     300,                  // Window height
                     1,                    // border width
-                    XCB_WINDOW_CLASS_INPUT_OUTPUT, //
+                    78,//XCB_WINDOW_CLASS_INPUT_OUTPUT, //
                     xcb.screen->root_visual,       //
                     valueMask, // Specify which values will be pased to server
                     values     // The actual values
   );
-  // NOTE: the window size and position may be ignored by some window managers
- 
+  // NOTE: The window size and position may be ignored by some window managers.
 
-  //
-  // Give window a Name
+  // Give the window a name
   const char* const wName = "Example 01";
   const uint32_t wNameLen = strlen(wName);
 
@@ -99,19 +132,31 @@ int main(void) {
   // Make the window visiable
   xcb_map_window(xcb.connection, window1);
 
-
-
-
-  xcb_flush(xcb.connection);
-
-
+  // Event loop
+    
   xcb_generic_event_t *event = nullptr;
 
 #define ESCAPE_KEYCODE 9
 
+  bool should_exit = false;
   while ((event = xcb_wait_for_event(xcb.connection))) {
 
-    if ((event->response_type & ~80) == XCB_KEY_PRESS) {
+    switch (event->response_type & ~80) {
+
+    // Case an xcb error has occured
+    case 0: { //Error
+        xcb_generic_error_t* error = (xcb_generic_error_t*) event;
+    
+        const char* const error_type = errorCodeToText(error->error_code);
+        const char* const opcode = opcodeToText(error->major_code);
+
+        fprintf(stderr, "XCB %s %s error. Minor opcode %d\n\n", opcode,
+                error_type, error->minor_code);
+        break;
+    }
+
+    // A key press event 
+    case XCB_KEY_PRESS: {
       // The event is a key press. Cast the event to a key press event
       xcb_key_press_event_t *press = (xcb_key_press_event_t *)event;
 
@@ -120,12 +165,21 @@ int main(void) {
 
       // If escape is pressed
       if (ESCAPE_KEYCODE == press->detail) {
-        free(event);
-        break;
+        should_exit = true;
       }
     }
 
+    default: {
+      break;
+    }
+
+    } // end switch
+
     free(event);
+
+    if (should_exit) {
+      break;
+    }
   }
 
   xcb_destroy_window(xcb.connection, window1);
