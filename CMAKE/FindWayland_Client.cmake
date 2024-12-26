f10668d590697944b77b8d28a3ab3ce153f5d11c
Ezekiel Holliday <thetinkeringengineer@protonmail.com>
2024-12-26 16:55:33 -0500

    Added a CMake find module for Wayland

---
+++ CMAKE/FindWayland_Client.cmake
@@ -0,0 +1,20 @@
+cmake_minimum_required(VERSION 3.20)
+# 
+# Defines
+#   WAYLAND_CLIENT_FOUND to TRUE  if Wayland client  libraries are found 
+#   WAYLAND_CLIENT_LIBRARIES      The library
+#   WAYLAND_CLIENT_LINK_LIBRARIES The full path to the library
+#   WAYLAND_CLIENT_INCLUDE_DIRS
+#  
+
+project("Fix CMAKE"
+    VERSION 0.0.1
+    LANGUAGES 
+    C 
+)
+
+find_package(PkgConfig)
+if(PKG_CONFIG_FOUND)
+    pkg_check_modules(WAYLAND_CLIENT QUIET wayland-client)
+endif()
+
